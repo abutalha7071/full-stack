283b833e1838aec9e7c1e99c9b6c5c8e1ef03459
abutalha7071 <abutalhazakikhan12345@gmail.com>
2023-11-01 10:16:13 +0530

    node

--- nodeJS_TUTE/Myserver.js
+++ nodeJS_TUTE/Myserver.js
@@ -4,3 +4,6 @@ const app=express()
 app.listen(1025,function(req,res){
     console.log("Server is Starated at 1025")
 })
+app.get("/",function(req,res){
+    res.send("Hi! This Is Abutalha Zaki")
+})
