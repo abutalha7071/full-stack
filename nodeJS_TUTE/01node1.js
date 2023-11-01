11c51564ac5ce255316f25a4c716190f9ee0a109
abutalha7071 <abutalhazakikhan12345@gmail.com>
2023-11-01 09:30:50 +0530

    node

---
+++ nodeJS_TUTE/01node1.js
@@ -0,0 +1,5 @@
+var p=1000;
+var r=4;
+var t=5;
+var si=(p*r*t)/1000;
+console.log("Simple Interest is "+si);
---
+++ nodeJS_TUTE/02_nodeDemo2.js
@@ -0,0 +1,4 @@
+// simple program to test the nodeJS
+n= "hello how are you"
+console.log(n)
+console.log("the length of the string"+ n.length)
